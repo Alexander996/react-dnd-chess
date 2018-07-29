09f2ce5d5409cc35f7ff52a085a3c4f3d84e38ae
Alexander Smirnov <al.smirnov996@gmail.com>
2018-07-30 00:00:24 +0300

    To form chessboard

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Square from "../Square";
 import Knight from "../Knight";
+import './style.css'
 
-export default class Board extends React.Component{
+export default class Board extends React.Component {
     static propTypes = {
         knightPosition: PropTypes.arrayOf(
             PropTypes.number.isRequired
@@ -11,10 +13,32 @@ export default class Board extends React.Component{
     };
 
     render() {
+        const squares = [];
+        for (let i = 0; i < 64; i++) {
+            squares.push(this.renderSquare(i))
+        }
+
         return (
-            <div>
-                <Square black>
-                    <Knight/>
+            <div className='board'>
+                {squares}
+            </div>
+        )
+    }
+
+    renderSquare(i) {
+        const x = i % 8;
+        const y = Math.floor(i / 8);
+        const black = (x + y) % 2 === 1;
+
+        const [knightX, knightY] = this.props.knightPosition;
+        const piece = (x === knightX && y === knightY) ?
+            <Knight/> :
+            null;
+
+        return (
+            <div key={i} className='square'>
+                <Square black={black}>
+                    {piece}
                 </Square>
             </div>
         )
